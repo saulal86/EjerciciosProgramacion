b705a87717e89361c201741289c7428d22e19fbd
saul <saulal86@educastur.es>
2024-02-14 12:38:47 +0100

    coleccionesII ejercicio7 y 8

--- src/ColeccionesII/Ejercicio6.java
+++ src/ColeccionesII/Ejercicio6.java
@@ -1,5 +1,59 @@
 package ColeccionesII;
 
-public class Ejercicio6 {
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 
+public class Ejercicio6 {
+    HashMap<Integer,Integer> map = new HashMap<>();
+    public int rellenaMapa(ArrayList<Integer> list){
+        if (!list.isEmpty()){
+            for (Integer n: list) {
+                if (map.containsKey(n)){
+                    int num = map.get(n);
+                    map.put(n,num+1);
+                }else{
+                    map.put(n,1);
+
+                }
+            }
+            return 1;
+        }
+        return 0;
+    }
+    public String moda(){
+        int max = 0;
+        int n = 0;
+        Iterator it = map.keySet().iterator();
+        while (it.hasNext()){
+            int numero = (Integer) it.next();
+            int value = map.get(numero);
+            if(value > max){
+                max = value;
+                n = numero;
+
+            }
+        }
+        return "la moda es " + n;
+    }
+
+
+    public static void main(String[] args) {
+        Ejercicio6 e = new Ejercicio6();
+        ArrayList<Integer> repeticiones = new ArrayList<>();
+        repeticiones.add(7, 5);
+        repeticiones.add(2, 1);
+        repeticiones.add(3, 6);
+        repeticiones.add(1, 2);
+        repeticiones.add(4, 1);
+        if( e.rellenaMapa(repeticiones)==0)
+        {
+            System.out.println("La moda es "+ 0);
+        }else {
+            System.out.println(e.moda());
+        }
+
+
+    }
 }
---
+++ src/ColeccionesII/Ejercicio7.java
@@ -0,0 +1,41 @@
+package ColeccionesII;
+
+import java.util.Stack;
+
+public class Ejercicio7 {
+
+    public void eliminarSiMayoresEncima(Stack<Integer> numeros){
+        Stack<Integer> pilaFinal = new Stack<>();
+        while(!numeros.isEmpty()){
+            int arriba = numeros.pop();
+            if(!numeros.isEmpty()) {
+                int abajo = numeros.peek();
+                if (arriba < abajo) {
+                    pilaFinal.add(arriba);
+                }else {
+                    pilaFinal.push(arriba);
+                    numeros.pop();
+                }
+            }
+        }
+        System.out.println(pilaFinal);
+    }
+
+    public static void main(String[] args) {
+        Ejercicio7 e = new Ejercicio7();
+        Stack<Integer> numeros = new Stack<>();
+        numeros.add(6);
+        numeros.add(22);
+        numeros.add(17);
+        numeros.add(10);
+        numeros.add(7);
+        numeros.add(9);
+        numeros.add(14);
+        numeros.add(5);
+        numeros.add(12);
+        numeros.add(7);
+        numeros.add(2);
+        e.eliminarSiMayoresEncima(numeros);
+    }
+
+}
